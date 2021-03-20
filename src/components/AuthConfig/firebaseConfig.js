91eef0e3d7a5a57c8fe939e7aa539ca0c52a3195
jagadishchakma <jagadishchakma401@gmail.com>
2021-03-19 20:29:32 -0700

    firebase auth

---
+++ src/components/AuthConfig/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDuKgTpZQBvA6ADN0Eklc5A97y1-eb9jEc",
+    authDomain: "bd-transportation-81a2d.firebaseapp.com",
+    projectId: "bd-transportation-81a2d",
+    storageBucket: "bd-transportation-81a2d.appspot.com",
+    messagingSenderId: "761904793451",
+    appId: "1:761904793451:web:5bfff7c1cecde75c2c6e2e"
+  };
+  export default firebaseConfig;
