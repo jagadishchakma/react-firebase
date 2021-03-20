25eb5db29a14e4290645cabb0b49605ae6a0e981
jagadishchakma <jagadishchakma401@gmail.com>
2021-03-19 20:35:06 -0700

    code

--- src/components/AuthConfig/firebaseConfig.js
+++ src/components/AuthConfig/firebaseConfig.js
@@ -1,9 +1,4 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDuKgTpZQBvA6ADN0Eklc5A97y1-eb9jEc",
-    authDomain: "bd-transportation-81a2d.firebaseapp.com",
-    projectId: "bd-transportation-81a2d",
-    storageBucket: "bd-transportation-81a2d.appspot.com",
-    messagingSenderId: "761904793451",
-    appId: "1:761904793451:web:5bfff7c1cecde75c2c6e2e"
+    // your config code
   };
   export default firebaseConfig;
